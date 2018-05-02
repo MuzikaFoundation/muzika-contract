83e405a583c5b7ef9bd2c1d4553036ef522b10d8
Jeong Chaehong <brcps12@gmail.com>
2018-05-02 19:51:19 +0900

    fix(migration): Add beta EIP712 (SignTypedData) schema (using for prefix before sign)

--- migrations/2_deploy_muzika_coin.js
+++ migrations/2_deploy_muzika_coin.js
@@ -1,11 +1,29 @@
+const ethUtil = require('ethereumjs-util');
+const ethAbi = require('ethereumjs-abi');
+const sigUtil = require('eth-sig-util');
+
 const MuzikaCoin = artifacts.require("MuzikaCoin");
 
 module.exports = (deployer) => {
 	const decimals = 18;
 	const initialSupply = 1e8;
 
+    let signedTypedDataSchema = [
+        "bytes8 Mode",
+        "address Token",
+        "address To",
+        "uint256 Amount",
+        "uint256 Fee",
+        "uint256 Nonce",
+    ];
+
+    let signedTypes = signedTypedDataSchema.map(v => v.split(' ')[0]);
+    let schemaTypes = signedTypedDataSchema.map(v => 'string');
+
+    let schemaPrefix = ethUtil.bufferToHex(ethAbi.soliditySHA3(schemaTypes, signedTypedDataSchema));
+
 	deployer.deploy(MuzikaCoin, initialSupply * (10 ** decimals)).then(() => {
 		const coinIns = MuzikaCoin.at(MuzikaCoin.address);
-		coinIns.upgradePrefixPreSignedSecond(3, '0x261f7a1df786c3890560458cad5bc0f68cc35032a1d6e147280ce4a9addac22d');
+		return coinIns.upgradePrefixPreSignedSecond(3, schemaPrefix);
 	});
 };
