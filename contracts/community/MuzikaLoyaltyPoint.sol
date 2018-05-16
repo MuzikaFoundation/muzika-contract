a6f7f5a51e4351f656fb80f69b4693fe4c980676
Jeong Chaehong <brcps12@gmail.com>
2018-05-16 14:28:02 +0900

    feat(LoyaltyPoint): create LoyaltyPoint Contract

---
+++ contracts/community/MuzikaLoyaltyPoint.sol
@@ -0,0 +1,66 @@
+pragma solidity ^0.4.23;
+
+import '../../zeppelin-solidity/contracts/ownership/Ownable.sol';
+import '../../zeppelin-solidity/contracts/math/SafeMath.sol';
+import '../token/MuzikaCoin.sol';
+
+contract MuzikaLoyaltyPoint is Ownable {
+  using SafeMath for uint256;
+
+  string public name = 'MUZIKA LOYALTY POINT';
+  string public symbol = 'MZKLP';
+  uint8 public decimals = 18;
+
+  MuzikaCoin internal _token = MuzikaCoin(0x9999888877776666555544443333222211110000);
+
+  uint8 public exchangeRatio = 50;
+
+  mapping (address => uint256) internal balances;
+
+  event Reward(address indexed to, uint256 amount);
+  event Exchange(address indexed to, uint256 fromAmount, uint256 toAmount);
+
+  function balanceOf(address _owner) public view returns (uint256) {
+    return balances[_owner];
+  }
+
+  function updateExchangeRatio(uint8 _ratio) public onlyOwner returns (bool) {
+    exchangeRatio = _ratio;
+    return true;
+  }
+
+  function reward(address _to, uint256 _amount) public onlyOwner returns (bool) {
+    balances[_to] = balances[_to].add(_amount);
+    emit Reward(_to, _amount);
+    return true;
+  }
+
+  function exchange(address _to) public onlyOwner returns (bool) {
+    require(balances[_to] > 0);
+    uint256 amount = balances[_to];
+    uint256 coinAmount = amount.div(100).mul(exchangeRatio);
+
+    balances[_to] = 0;
+    _token.transfer(_to, coinAmount);
+
+    emit Exchange(_to, amount, coinAmount);
+    return true;
+  }
+
+  function exchangeFromAddresses(address[] _people) public onlyOwner returns(bool) {
+    for(uint8 i = 0; i < _people.length; i++) {
+      address _to = _people[i];
+      if (balances[_to] > 0) {
+        uint256 amount = balances[_to];
+        uint256 coinAmount = amount.div(100).mul(exchangeRatio);
+
+        balances[_to] = 0;
+        _token.transfer(_to, coinAmount);
+
+        emit Exchange(_to, amount, coinAmount);
+      }
+    }
+
+    return true;
+  }
+}
---
+++ test/MuzikaLoyaltyPoint.test.js
@@ -0,0 +1,51 @@
+import {promisify} from './helpers/promisify';
+
+const BigNumber = web3.BigNumber;
+const MuzikaCoin = artifacts.require('MuzikaCoin');
+const PreSignedContract = artifacts.require('PreSignedContract');
+const MuzikaLoyaltyPoint = artifacts.require('MuzikaLoyaltyPoint');
+
+require('chai')
+  .use(require('chai-as-promised'))
+  .use(require('chai-bignumber')(BigNumber))
+  .should();
+
+contract('MuzikaCoin', ([_, owner, recipient, anotherAccount]) => {
+  const initialSupply = 10000;
+  const exchangeRatio = 80;
+
+  let token;
+  let loyalty;
+
+  let backedUpByteCode = MuzikaLoyaltyPoint.unlinked_binary;
+
+  afterEach(() => {
+    // Restore bytecode of paper contract
+    MuzikaLoyaltyPoint.unlinked_binary = backedUpByteCode;
+  });
+
+  beforeEach(async () => {
+    token = await MuzikaCoin.new(initialSupply, {from: owner});
+
+    /* Manually replace address of PreSignedContract in compile stage
+     * So, you should not be input to the constructor on contracts
+     */
+    MuzikaLoyaltyPoint.unlinked_binary = MuzikaLoyaltyPoint.unlinked_binary
+      .replace('9999888877776666555544443333222211110000', token.address.slice(2));
+
+    loyalty = await MuzikaLoyaltyPoint.new({from: owner});
+
+    await loyalty.updateExchangeRatio(exchangeRatio, {from: owner});
+    await token.transfer(loyalty.address, 5000, {from: owner});
+  });
+
+  it('exchanges from loyalty points to muzika coin', async () => {
+    const lpAmount = 1000;
+    await loyalty.reward(recipient, lpAmount, {from: owner});
+    await loyalty.exchange(recipient, {from: owner});
+
+    const balance = await token.balanceOf(recipient);
+
+    balance.should.be.bignumber.equal(lpAmount / 100 * exchangeRatio);
+  });
+});
