4caa49e89295d3232d388946cfc6ce3c40bb6763
Jeong Chaehong <brcps12@gmail.com>
2018-05-16 12:39:44 +0900

    refactor(MuzikaCoin): remove PreSigned functions and variable, and add PreSignedContract externally

--- contracts/token/MuzikaCoin.sol
+++ contracts/token/MuzikaCoin.sol
@@ -2,16 +2,12 @@ pragma solidity ^0.4.23;
 
 import '../../zeppelin-solidity/contracts/token/ERC20/MintableToken.sol';
 import '../../zeppelin-solidity/contracts/lifecycle/Pausable.sol';
+import '../lib/PreSignedContract.sol';
 
 contract MuzikaCoin is MintableToken, Pausable {
   string public name = 'MUZIKA COIN';
   string public symbol = 'MZK';
   uint8 public decimals = 18;
-
-  bytes8 constant internal MODE_TRANSFER = 'Transfer';
-  bytes8 constant internal MODE_APPROVAL = 'Approval';
-  bytes8 constant internal MODE_INC_APPROVAL = 'IncApprv';
-  bytes8 constant internal MODE_DEC_APPROVAL = 'DecApprv';
 
   event Burn(address indexed burner, uint256 value);
 
@@ -37,8 +33,7 @@ contract MuzikaCoin is MintableToken, Pausable {
 
   mapping (bytes => bool) internal _signatures;
 
-  mapping (uint8 => bytes) internal _prefixPreSignedFirst;
-  mapping (uint8 => bytes) internal _prefixPreSignedSecond;
+  PreSignedContract internal _preSignedContract = PreSignedContract(0x1111222233334444555566667777888899990000);
 
   modifier onlyNotFrozenAddress(address _target) {
     require(!frozenAddress[_target]);
@@ -54,20 +49,6 @@ contract MuzikaCoin is MintableToken, Pausable {
     totalSupply_ = initialSupply;
     balances[msg.sender] = initialSupply;
     emit Transfer(0x0, msg.sender, initialSupply);
-  }
-
-  function upgradePrefixPreSignedFirst(uint8 _version, bytes _prefix)
-    public
-    onlyOwner
-  {
-    _prefixPreSignedFirst[_version] = _prefix;
-  }
-
-  function upgradePrefixPreSignedSecond(uint8 _version, bytes _prefix)
-    public
-    onlyOwner
-  {
-    _prefixPreSignedSecond[_version] = _prefix;
   }
 
   /**
@@ -203,8 +184,8 @@ contract MuzikaCoin is MintableToken, Pausable {
     require(_to != address(0));
     require(_signatures[_sig] == false);
 
-    address _from = preSignedCheck(
-      MODE_TRANSFER,
+    address _from = _preSignedContract.transferPreSignedCheck(
+      address(this),
       _to,
       _value,
       _fee,
@@ -245,8 +226,8 @@ contract MuzikaCoin is MintableToken, Pausable {
   {
     require(_signatures[_sig] == false);
 
-    address _from = preSignedCheck(
-      MODE_APPROVAL,
+    address _from = _preSignedContract.approvePreSignedCheck(
+      address(this),
       _to,
       _value,
       _fee,
@@ -286,8 +267,8 @@ contract MuzikaCoin is MintableToken, Pausable {
   {
     require(_signatures[_sig] == false);
 
-    address _from = preSignedCheck(
-      MODE_INC_APPROVAL,
+    address _from = _preSignedContract.increaseApprovalPreSignedCheck(
+      address(this),
       _to,
       _value,
       _fee,
@@ -327,8 +308,8 @@ contract MuzikaCoin is MintableToken, Pausable {
   {
     require(_signatures[_sig] == false);
 
-    address _from = preSignedCheck(
-      MODE_DEC_APPROVAL,
+    address _from = _preSignedContract.decreaseApprovalPreSignedCheck(
+      address(this),
       _to,
       _value,
       _fee,
@@ -358,154 +339,5 @@ contract MuzikaCoin is MintableToken, Pausable {
     emit ApprovalPreSigned(_from, _to, msg.sender, oldValue, _fee);
 
     return true;
-  }
-
-  function messagePreSignedHashing(
-    bytes8 _mode,
-    address _token,
-    address _to,
-    uint256 _value,
-    uint256 _fee,
-    uint256 _nonce,
-    uint8 _version
-  )
-    public view returns (bytes32 hash)
-  {
-    if (_version <= 2) {
-      hash = keccak256(
-        _mode,
-        _token,
-        _to,
-        _value,
-        _fee,
-        _nonce
-      );
-    } else {
-      // Support SignTypedData flexibly
-      hash = keccak256(
-        _prefixPreSignedFirst[_version],
-        _mode,
-        _token,
-        _to,
-        _value,
-        _fee,
-        _nonce
-      );
-    }
-  }
-
-  function preSignedHashing(
-    bytes8 _mode,
-    address _token,
-    address _to,
-    uint256 _value,
-    uint256 _fee,
-    uint256 _nonce,
-    uint8 _version
-  )
-    public view returns (bytes32)
-  {
-    bytes32 hash = messagePreSignedHashing(
-      _mode,
-      _token,
-      _to,
-      _value,
-      _fee,
-      _nonce,
-      _version
-    );
-
-    if (_version <= 2) {
-      if (_version == 0) {
-        return hash;
-      } else if (_version == 1) {
-        return keccak256(
-          '\x19Ethereum Signed Message:\n32',
-          hash
-        );
-      } else {
-        // Support Standard Prefix (Trezor)
-        return keccak256(
-          '\x19Ethereum Signed Message:\n\x20',
-          hash
-        );
-      }
-    } else {
-      // Support SignTypedData flexibly
-      if (_prefixPreSignedSecond[_version].length > 0) {
-        return keccak256(
-          _prefixPreSignedSecond[_version],
-          hash
-        );
-      } else {
-        return hash;
-      }
-    }
-  }
-
-  function preSignedCheck(
-    bytes8 _mode,
-    address _to,
-    uint256 _value,
-    uint256 _fee,
-    uint256 _nonce,
-    uint8 _version,
-    bytes _sig
-  )
-  internal view returns (address)
-  {
-    bytes32 hash = preSignedHashing(
-      _mode,
-      address(this),
-      _to,
-      _value,
-      _fee,
-      _nonce,
-      _version
-    );
-
-    address _from = recover(hash, _sig);
-    require(_from != address(0));
-
-    return _from;
-  }
-
-  /**
-   * @dev Recover signer address from a message by using their signature
-   * @param hash bytes32 message, the hash is the signed message. What is recovered is the signer address.
-   * @param sig bytes signature, the signature is generated using web3.eth.sign()
-   */
-  function recover(bytes32 hash, bytes sig) internal pure returns (address) {
-    bytes32 r;
-    bytes32 s;
-    uint8 v;
-
-    // Check the signature length
-    if (sig.length != 65) {
-      return (address(0));
-    }
-
-    // Divide the signature in r, s and v variables
-    // ecrecover takes the signature parameters, and the only way to get them
-    // currently is to use assembly.
-    // solium-disable-next-line security/no-inline-assembly
-    assembly {
-      r := mload(add(sig, 32))
-      s := mload(add(sig, 64))
-      v := byte(0, mload(add(sig, 96)))
-    }
-
-    // Version of signature should be 27 or 28, but 0 and 1 are also possible versions
-    if (v < 27) {
-      v += 27;
-    }
-
-    // If the version is correct return the signer address
-    if (v != 27 && v != 28) {
-      return (address(0));
-    } else {
-      // solium-disable-next-line arg-overflow
-      return ecrecover(hash, v, r, s);
-    }
   }
 }
