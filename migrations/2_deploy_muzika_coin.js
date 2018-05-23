ac47e8858b197a540a0ca76dc6b8cb7493f6bb65
Jeong Chaehong <brcps12@gmail.com>
2018-05-23 17:38:16 +0900

    upgrade: upgrade truffle version to 4.1.9

--- migrations/2_deploy_muzika_coin.js
+++ migrations/2_deploy_muzika_coin.js
@@ -10,9 +10,7 @@ module.exports = (deployer) => {
   const decimals = 18;
   const initialSupply = 1e8;
 
-  deployer.deploy(PreSignedContract).then(() => {
-    return PreSignedContract.deployed();
-  }).then(preSignedContract => {
+  deployer.deploy(PreSignedContract).then(preSignedContract => {
     let signedTypedDataSchema = [
       "bytes8 Mode",
       "address Token",
