2a69f3b079f44c2c1f10266b504158303eaea85a
Jeong Chaehong <brcps12@gmail.com>
2018-05-23 17:51:11 +0900

    fix(contract): lint fix

--- contracts/lib/Dispatcher.sol
+++ contracts/lib/Dispatcher.sol
@@ -1,12 +1,13 @@
 pragma solidity ^0.4.24;
 
-import "./DispatcherStorage.sol";
+import './DispatcherStorage.sol';
 
 contract Dispatcher {
   function() public {
     DispatcherStorage dispatcherStorage = DispatcherStorage(0x1111222233334444555566667777888899990000);
     address target = dispatcherStorage.lib();
 
+    // solium-disable-next-line security/no-inline-assembly
     assembly {
       calldatacopy(0x0, 0x0, calldatasize)
       let success := delegatecall(sub(gas, 10000), target, 0x0, calldatasize, 0x0, 0)
--- contracts/lib/DispatcherStorage.sol
+++ contracts/lib/DispatcherStorage.sol
@@ -1,6 +1,6 @@
 pragma solidity ^0.4.24;
 
-import "../../zeppelin-solidity/contracts/ownership/Ownable.sol";
+import '../../zeppelin-solidity/contracts/ownership/Ownable.sol';
 
 contract DispatcherStorage is Ownable {
   address public lib;
