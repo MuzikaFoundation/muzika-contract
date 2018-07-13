3c05130e49d9d91ca0ca4d255fce6134e7038877
Jeong Chaehong <brcps12@gmail.com>
2018-07-13 16:17:33 +0900

    fix(MuzikaCoin): remove useless modifier and functions

--- contracts/token/MuzikaCoin.sol
+++ contracts/token/MuzikaCoin.sol
@@ -12,55 +12,10 @@ contract MuzikaCoin is MintableToken, Pausable {
 
   event Burn(address indexed burner, uint256 value);
 
-  event FreezeAddress(address indexed target);
-  event UnfreezeAddress(address indexed target);
-
-  mapping (address => bool) public frozenAddress;
-
-  modifier onlyNotFrozenAddress(address _target) {
-    require(!frozenAddress[_target]);
-    _;
-  }
-
-  modifier onlyFrozenAddress(address _target) {
-    require(frozenAddress[_target]);
-    _;
-  }
-
   constructor(uint256 initialSupply) public {
     totalSupply_ = initialSupply;
     balances[msg.sender] = initialSupply;
     emit Transfer(address(0), msg.sender, initialSupply);
-  }
-
-  /**
-   * @dev Freeze account(address)
-   *
-   * @param _target The address to freeze
-   */
-  function freezeAddress(address _target)
-    public
-    onlyOwner
-    onlyNotFrozenAddress(_target)
-  {
-    frozenAddress[_target] = true;
-
-    emit FreezeAddress(_target);
-  }
-
-  /**
-   * @dev Unfreeze account(address)
-   *
-   * @param _target The address to unfreeze
-   */
-  function unfreezeAddress(address _target)
-    public
-    onlyOwner
-    onlyFrozenAddress(_target)
-  {
-    delete frozenAddress[_target];
-
-    emit UnfreezeAddress(_target);
   }
 
   /**
@@ -82,76 +37,15 @@ contract MuzikaCoin is MintableToken, Pausable {
     emit Transfer(_who, address(0), _value);
   }
 
-  function transfer(
-    address _to,
-    uint256 _value
-  )
-    public
-    onlyNotFrozenAddress(msg.sender)
-    whenNotPaused
-    returns (bool)
-  {
+  function transfer(address _to, uint256 _value) public whenNotPaused returns (bool) {
     return super.transfer(_to, _value);
   }
 
-  function transferFrom(
-    address _from,
-    address _to,
-    uint256 _value
-  )
-    public
-    onlyNotFrozenAddress(_from)
-    onlyNotFrozenAddress(msg.sender)
-    whenNotPaused
-    returns (bool)
-  {
+  function transferFrom(address _from, address _to, uint256 _value) public whenNotPaused returns (bool) {
     return super.transferFrom(_from, _to, _value);
   }
 
-  function approve(
-    address _spender,
-    uint256 _value
-  )
-    public
-    onlyNotFrozenAddress(msg.sender)
-    whenNotPaused
-    returns (bool)
-  {
-    return super.approve(_spender, _value);
-  }
-
-  function increaseApproval(
-    address _spender,
-    uint _addedValue
-  )
-    public
-    onlyNotFrozenAddress(msg.sender)
-    whenNotPaused
-    returns (bool)
-  {
-    return super.increaseApproval(_spender, _addedValue);
-  }
-
-  function decreaseApproval(
-    address _spender,
-    uint _subtractedValue
-  )
-    public
-    onlyNotFrozenAddress(msg.sender)
-    whenNotPaused
-    returns (bool)
-  {
-    return super.decreaseApproval(_spender, _subtractedValue);
-  }
-
-  function increaseApprovalAndCall(
-    address _spender,
-    uint _addedValue,
-    bytes _data
-  )
-    public
-    returns (bool)
-  {
+  function increaseApprovalAndCall(address _spender, uint _addedValue, bytes _data) public returns (bool) {
     require(_spender != address(this));
 
     increaseApproval(_spender, _addedValue);
