6ba073fe3c43f4480d33d520526346a9647cc340
Jeong Chaehong <brcps12@gmail.com>
2018-08-03 20:02:45 +0900

    fix(project): support coverage

--- truffle-config.js
+++ truffle-config.js
@@ -12,6 +12,13 @@ module.exports = {
       port: 8545,
       network_id: '*'
     },
+    coverage: {
+      host: '127.0.0.1',
+      network_id: '*', // eslint-disable-line camelcase
+      port: 8555,
+      gas: 0xfffffffffff,
+      gasPrice: 0x01,
+    },
     mainnet: {
       provider: newProvider('mainnet'),
       network_id: 1,
